2bf5e4d6414a62cc380bc8a2d8e19401679d248e
hugomflavio <hmflavio92@gmail.com>
2021-08-12 14:45:17 -0400

    make online copy of adp script

---
+++ NKA/Protocol_scripts/R_adp_standards.R
@@ -0,0 +1,27 @@
+# ------------------------------------------
+# R-script to calculate the standard curve
+# ------------------------------------------
+
+# start by storing the absorbance values in a csv 
+# containing two columns: 'ADP' and 'Abs'
+# Name the file 'adp_standards.csv' then run:
+
+source('https://git.io/JRSJs')
+# This line downloads the function adp_standard, which we will use below.
+
+adp_points <- read.csv('adp_standards.csv')
+adp_standard(adp_points)
+
+# What just happened?
+#
+# We are calculating two models. One using all the points, and another using 
+# the means for each standard value. The R2 of the mean model gives us an idea 
+# of how good our standard curve is. We want it above 0.95.
+#
+# On the other hand, the R2 of the point model tells us if there is a lot of 
+# dispersion between replicates. If the R2 of the m_all model is low, (i.e. 
+# under 0.9), then you may need to practice consistent pipetting a bit more.
+#
+# The equation for both models is the same.
+#
+# Save the plot, the R2 of both models, and model equation.
